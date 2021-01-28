15c7265ed7dd88e1b3323a05f06806d86bd54ca3
Abuu001 <abulugz@gmail.com>
2021-01-28 23:43:36 +0300

    add route handler

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,7 @@ router.post('/api/login',userProfileUpload,controller.userLogin)
 router.get('/api/users-list/:id',controller.getUserList)
 router.get('/api/user/:id',controller.getUserInfo)
 router.post('/api/chats',controller.getUserChats)
-router.get('/api/user/is-offline/:id',controller.checkIfUserOffline)
+// router.get('/api/user/is-offline/:id',controller.checkIfUserOffline)
 router.post('/api/upload-voice',audioUpload,controller.uploadVoice)
 router.post('/api/upload-image-file',imageMgsFileUpload,controller.uploadImageFile)
 
