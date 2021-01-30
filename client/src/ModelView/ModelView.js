6d7fcfeee16e61f78e1c170b98500e0f67502ce8
Abuu001 <abulugz@gmail.com>
2021-01-30 11:45:43 +0300

    chanced format

---
+++ client/src/ModelView/ModelView.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "./ModelView.css"
+import SearchPeople from "../SideBar/SearchPeople";
+import ProfileSection from "../SideBar/ProfileSection";
+import ChatCardsListing from "../SideBar/ChatCardListing"
+import ChatSection from "../ChatSection/ChatSection";
+
+function ModelView({authInfo,setCount,setAuthInfo}) {
+  return (
+      <div className="App">
+        <div className="left__Side">
+          <ProfileSection setCount={setCount} authInfo={authInfo} setAuthInfo={setAuthInfo}/>
+            <SearchPeople />
+          <ChatCardsListing authInfo={authInfo} /> 
+        </div>
+
+      <div className="right__Side">
+        <ChatSection authInfo={authInfo} />
+      </div>
+    </div>
+   
+  );
+}
+
+export default ModelView;
