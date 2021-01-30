3cd48852995cb7db9f2c24dfcace82dc96b7686d
Abuu001 <abulugz@gmail.com>
2021-01-30 11:47:21 +0300

    added firebase utils

---
+++ client/src/utils/apiEndpoints.js
@@ -0,0 +1,7 @@
+export const BASE_URL="http://localhost:3002";
+export const LOGIN ="/api/login";
+export const USER_LIST ="/api/users-list"
+export const USER ="/api/chats";
+export const  UPLOAD_AUDIO ="/api/upload-voice"
+export const UPLOAD_IMAGE_FILE = "/api/upload-image-file"
+ 
---
+++ client/src/utils/firebase.js
@@ -0,0 +1,29 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAbWfhJEv030vkb0YjhgVMQJQ_Ywd4k_Lg",
+    authDomain: "whatsappyoclone.firebaseapp.com",
+    projectId: "whatsappyoclone",
+    storageBucket: "whatsappyoclone.appspot.com",
+    messagingSenderId: "30472321078",
+    appId: "1:30472321078:web:221ce3cece3893d9180097",
+    measurementId: "G-1NXDECZ8LV"
+};
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyAbWfhJEv030vkb0YjhgVMQJQ_Ywd4k_Lg",
+    authDomain: "whatsappyoclone.firebaseapp.com",
+    projectId: "whatsappyoclone",
+    storageBucket: "whatsappyoclone.appspot.com",
+    messagingSenderId: "30472321078",
+    appId: "1:30472321078:web:221ce3cece3893d9180097",
+    measurementId: "G-1NXDECZ8LV"
+})
+const auth= app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+  
+export   {
+    firebaseConfig,
+    provider,
+    auth
+};
