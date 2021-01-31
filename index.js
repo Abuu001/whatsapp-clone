34759198674d9b448ad3704266891b013d36b108
Abuu001 <abulugz@gmail.com>
2021-01-31 10:46:27 +0300

    register router

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ const app =express()
 const http= require('http')
 const server = http.createServer(app)
 const cors =require('cors')
-const routes=require('./routes/routes')
+const router=require('./routes/routes')
 
 //middlewares
 app.use(cors())
-app.use(routes);
 app.use(express.static("uploads"),)
 app.use(express.json())
 
 
-const io = (module.exports.io =require('socket.io')(server))
-const socketManager = require('./socketManager/socketManager')
-io.on("connection",socketManager)
+app.use("/api/v1",router)
 
 const PORT= process.env.PORT || 3004;
 server.listen(PORT,()=>console.log(`Server Running in port ${PORT}`));
