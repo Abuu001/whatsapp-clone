f25f0192a95ed6b1b1d36ec6df9bdd9d678d5356
Abuu001 <abulugz@gmail.com>
2021-02-06 21:49:37 +0300

    Add flip move

--- client/src/ChatSection/Chat.js
+++ client/src/ChatSection/Chat.js
@@ -1,34 +1,25 @@
 import React,{useState,useEffect} from 'react'
 import "./Chat.css"
 import axios from "axios"
-import moment from "moment"
 import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 import Pusher from "pusher-js";
+import FlipMove from 'react-flip-move';
 
-
-function Chat({authInfo}) {
+const Chat= ({authInfo})=> {
     const [messages,setMessages]=useState([])
 
     const getMessages=async()=>{
-        await axios.get("/api/v1/messages")
+       await  axios.get("/api/v1/messages")
         .then(response=>{
-            const gg =response.data.message
-            console.log(gg);
-            // gg.map(tt=>console.log(tt))  
-       
-            // setMessages([...response.data.message])
             setMessages([...response.data.message])
         })
     }
 
-    const deleteMsgHandler=async(id)=>{
-        await axios.delete(`/api/v1/messages/${id}`)
-        console.log("deletee");
-        toast.warning("Message deleted !!!",{position:"top-right"})
-        // messages.pop()
-        // setMessages([...messages])
-    }
+    const deleteMsgHandler= async(id)=>{
+       // toast.warning("Message deleted !!!",{position:"top-right"})
+     await axios.delete(`/api/v1/messages/${id}`)
+    } 
  
     useEffect(()=>{
         getMessages() 
@@ -45,10 +36,8 @@ function Chat({authInfo}) {
            channel.bind('new_record', (newMessage)=> { 
                 
             //   alert(JSON.stringify(newMessage))  
-              console.log(newMessage);
               messages.push(newMessage)
               setMessages([...messages])
-       
         });
  
         //for delete
@@ -57,14 +46,9 @@ function Chat({authInfo}) {
             encrypted: true
           });
         
-           const channel_delete = pusher_delete.subscribe('watch_whatsappmainmessages_delete');
-           channel.bind('delete', (newMessage)=> { 
-                
-              alert(JSON.stringify(newMessage))  
-            //   console.log(newMessage);
-            //   messages.push(newMessage)
-              setMessages([...newMessage])
-       
+        const channel_delete = pusher_delete.subscribe('watch_whatsappmainmessages_delete');
+            pusher_delete.bind('delete', (newMessage)=> { 
+            setMessages(newMessage)
         });
         return()=>{  
             channel.unbind_all()
@@ -75,51 +59,25 @@ function Chat({authInfo}) {
     },[messages])
 
     const msgLoop=messages.map(msg=>{
-        // const date = new Date(parseInt(msg.time_sent));
-        // const localeSpecificTime = date.toLocaleTimeString();
-
-        // var date = new Date(parseInt(msg.time_sent));
-        // const localeSpecificTime = date.toLocaleTimeString(navigator.language, {
-        //         hour: '2-digit',
-        //         minute:'2-digit'
-        //     });
-
         return(
-              <div className= {`${authInfo[0].additionalUserInfo?.profile.given_name  !== msg.name ?   `${'chat you'}` : `${'chat me'}` }`}  key={msg.id} >
-                    <span className="name">
-                    {/* {authInfo[0].additionalUserInfo?.profile.given_name } */}
-                    {msg.name}
-                    </span>
-                    <p className="msg">{msg.messages}</p>
-                    {/* <span className="time">{msg.time_sent}</span>  */}
-                    <span className="time">{msg.time_sent}</span> 
-                    <CancelOutlinedIcon fontSize="small"  className="cancelBtn" onClick={()=>deleteMsgHandler(msg.id)}/>
-               </div>  
+            <div className= {`${authInfo[0].additionalUserInfo?.profile.given_name  !== msg.name ?   `${'chat you'}` : `${'chat me'}` }`}  key={msg.id} >
+                <span className="name">
+                {msg.name}
+                </span>
+                <p className="msg">{msg.messages}</p>
+                <span className="time">{msg.time_sent}</span> 
+                <CancelOutlinedIcon fontSize="small"  className="cancelBtn" onClick={()=>deleteMsgHandler(msg.id)}/>
+            </div>  
         )
     })
-
-    return ( 
-        <div className="chat__Section">
-            <div className="chat you">
-                <span className="name">
-                  Martha
-                </span>
-            <p className="msg">message</p>
-            <span className="time">10:50 PM</span> 
-            </div>
  
-            <div className="chat me">
-                <span className="name">
-                {authInfo[0].additionalUserInfo?.profile.given_name }
-                </span>
-                 <p className="msg">Niaje bro</p>
-                <span className="time">10:50 PM</span> 
-            </div>
-       
-            {msgLoop} 
-
+    return ( 
+        <div className="chat__Section"  >
+            <FlipMove >
+               {msgLoop}             
+            </FlipMove>
         </div>
     )
-}
+};
 
 export default Chat
