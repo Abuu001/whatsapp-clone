f8f98e97622eb80f77255af433f7d983323f1b05
Abuu001 <abulugz@gmail.com>
2021-02-09 23:44:07 +0300

    modified

--- client/src/ChatSection/ChatForm.js
+++ client/src/ChatSection/ChatForm.js
@@ -6,10 +6,12 @@ import Picker from 'emoji-picker-react';
 import IconButton from '@material-ui/core/IconButton';
 import moment from "moment"
 
+
 function ChatForm({authInfo}) {
  
     const [sendMsg,setSendMsg]=useState('')
-    const [sendFile,setSendFile]=useState([])
+    const [sendFile,setSendFile]=useState("")
+    const [record,setRecord]=useState(false)
     const [openEmojiPicker,setOpenEmojiPicker]=useState(false)
     const [disableSubmitBtn,setDisableSubmitBtn]=useState(true) 
 
@@ -19,10 +21,11 @@ function ChatForm({authInfo}) {
         setSendMsg(msg);
     };
 
-    const submitMsgHandler=async()=>{
+    const submitMsgHandler=()=>{
 
-   //if there !text => dont send empty msgs
-        if(!sendMsg.length <1 && !sendMsg !==''){
+        //if there !text => dont send empty msgs
+        if(!sendMsg.length <1 && !sendMsg !=='' ){
+
             const currentTime =moment().format('LT');
             const data={
                 name : authInfo[0].additionalUserInfo?.profile.given_name,
@@ -30,31 +33,55 @@ function ChatForm({authInfo}) {
                 messages : sendMsg,
                 time_sent : currentTime
             }
-       await  axios.post("/api/v1/messages",data)
-           
+         axios.post("/api/v1/messages",data) 
+        }
+
+        //for sending files
+        if(sendFile !==''){
+            const formData = new FormData();
+            
+            // gets the name of the file to be uploaded
+            formData.append('name' ,sendFile.name);
+            formData.append('file' ,sendFile);
+ 
+          //setSendFile(formData)
+             
+            console.log(sendFile); 
+            console.log(formData); 
+            const   fileConfig={
+                headers:{
+                    'Content-Type' : 'multipart/form-data'
+                }
+            }
+        
+         axios.post("/api/v1/uploads",formData,fileConfig)
         }
 
         setSendMsg('') 
-        setSendFile('')
+        setSendFile(null)
         setOpenEmojiPicker(false)
     } 
+    
     
     useEffect(()=>{   
        submitMsgHandler()
     },[])           
-    
+ 
     return ( 
         <>
-                {openEmojiPicker ?  <Picker onEmojiClick={onEmojiClick}/>  :  null}
+              {openEmojiPicker ?  <Picker onEmojiClick={onEmojiClick}/>  :  null}
             <div className="chartForm">
                 <div className="action__Butn">
                     <i className="far fa-smile form__Icon"  value={sendMsg} onClick={()=>setOpenEmojiPicker(prevState=>!prevState)}></i> 
-                    <i className="fas fa-paperclip form__Icon"  value={sendFile} onChange={e=>setSendFile(e)}></i>
+                  <span className="file__clip"> 
+                        <input  type="file" name="image" id="file__Input"  accept=".jpg" onChange={e=>setSendFile(e.target.files[0])}/>
+                        <i className="fas fa-paperclip form__Icon file__clip__Icon"  ></i>
+                  </span>
                 </div>
                     <input className="chat__Input" placeholder="message" name="name" value={sendMsg} onChange={e=>setSendMsg(e.target.value)}/>  <IconButton onClick={submitMsgHandler}  >< SendIcon /></IconButton>
-                    <i className="fas fa-microphone  form__Icon form__Icon__active" value={sendFile} onChange={e=>setSendFile(e)}></i>
-            </div>
-        </>
+                   <i className="fas fa-microphone  form__Icon form__Icon__active"onChange={e=>setSendFile(e)}></i>
+            </div> 
+        </> 
     )
 }
 // className={`${disableSubmitBtn ===true ?  'btn__disable' :  'btn__enable'  }`}  
