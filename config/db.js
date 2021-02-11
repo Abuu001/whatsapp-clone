38a2d6cdf31e52f0969dfa6ad9c9ae1ae9bb7ab6
Abuu001 <abulugz@gmail.com>
2021-02-11 23:37:19 +0300

    ready for heroku

--- config/db.js
+++ config/db.js
@@ -10,11 +10,10 @@ require('dotenv').config({path : "./dev.env"});
 //     port:  process.env.PG_PORT
 // })
 
-//const devConfig=`postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
-const devConfig = `postgres://ojafszxdrsoysa:94114e55b616e5c6c481c40e77473b7d377659a1421f9d1ff2bd99d4d148a149@ec2-54-164-241-193.compute-1.amazonaws.com:5432/d84frempg8coe6`
+const devConfig=`postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 const prodConfig = process.env.DATABASE_URL
 const pool = new Pool({
-    connectionString : process.env.NODE_ENV === "production" ? prodConfig : devConfig
+    connectionString : process.env.NODE_ENV !=='production' ?  devConfig : prodConfig
 });
  
 module.exports =pool;
