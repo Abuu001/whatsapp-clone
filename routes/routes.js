b578c4a1749b0b9486ecb2537c7d78b4bbe8af84
Abuu001 <abulugz@gmail.com>
2021-02-11 22:58:01 +0300

    ready for heroku

--- routes/routes.js
+++ routes/routes.js
@@ -28,7 +28,7 @@ pool.connect(async(err,client)=>{
 //get messages api
 router.get('/messages',async(req,res)=>{
     try {
-        const response = await pgClient.query("SELECT * FROM whatsappmainmessages  ORDER BY  groups ASC ");
+        const response = await pool.query("SELECT * FROM whatsappmainmessages  ORDER BY  groups ASC ");
         // const response = await pgClient.query("SELECT messages,groups,groups_id,images FROM whatsappmainmessages RIGHT JOIN whatsappimages ON whatsappimages.groups_id = whatsappmainmessages.groups ORDER BY  groups ASC");
 
         res.status(200).json({
@@ -46,7 +46,7 @@ router.get('/messages',async(req,res)=>{
 router.post('/messages',async(req,res)=>{
     try {
         const {name,messages,time_sent}= req.body;
-        const response = await pgClient.query("INSERT INTO whatsappmainmessages (messages,time_sent,name)  VALUES($1,$2,$3) RETURNING *",[messages,time_sent,name])
+        const response = await pool.query("INSERT INTO whatsappmainmessages (messages,time_sent,name)  VALUES($1,$2,$3) RETURNING *",[messages,time_sent,name])
 
         if(req.method === 'POST'){
             await pusher.trigger('watch_whatsappmainmessages', 'new_record', response.rows[0]);
@@ -69,8 +69,8 @@ router.post('/messages',async(req,res)=>{
 router.delete('/messages/:id',async(req,res)=>{
     try {
         const {id}= req.params  ;
-        const response = await pgClient.query("DELETE  FROM whatsappmainmessages WHERE id=$1 ",[id])
-        const messges = await pgClient.query("SELECT * FROM whatsappmainmessages  ORDER BY  groups ASC "); 
+        const response = await pool.query("DELETE  FROM whatsappmainmessages WHERE id=$1 ",[id])
+        const messges = await pool.query("SELECT * FROM whatsappmainmessages  ORDER BY  groups ASC "); 
     
         if(req.method==='DELETE'){  
             await pusher.trigger('watch_whatsappmainmessages_delete', 'delete', messges.rows); 
