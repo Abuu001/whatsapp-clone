58faa75be02b72ed60c16f9b1c8d5d4c3a00774e
Abuu001 <abulugz@gmail.com>
2021-02-13 14:05:26 +0300

    md

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
-const {Pool}=require('pg');
-require('dotenv').config({path : "./dev.env"});
-
+const { Pool } = require("pg");
+require("dotenv").config({ path: "./dev.env" });
 
 // const pool = new Pool({
 //     user : process.env.PG_USER,
@@ -10,16 +9,12 @@ require('dotenv').config({path : "./dev.env"});
 //     port:  process.env.PG_PORT
 // })
 
-const devConfig={
-    connectionString : `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
-}
-const prodConfig = {
-    connectionString : process.env.DATABASE_URL
-}
+const devConfig = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const prodConfig = process.env.DATABASE_URL;
 
-const pool = new Pool(
-   // connectionString : process.env.NODE_ENV ==='production' ?  prodConfig : devConfig 
-   process.env.NODE_ENV ==='production' ?  prodConfig : devConfig 
-); 
-  
-module.exports =pool;
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? prodConfig : devConfig,
+});
+
+module.exports = pool;
